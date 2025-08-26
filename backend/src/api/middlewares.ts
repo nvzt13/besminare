d176878a6c5e5f8a6e14b3b682503af851e2eed8
Nevzat Atalay <nevzatatalay79@gmail.com>
2025-08-26 21:58:51 +0300

    extend brand features

--- backend/src/api/middlewares.ts
+++ backend/src/api/middlewares.ts
@@ -3,7 +3,14 @@ import {
   validateAndTransformBody,
 } from "@medusajs/framework/http"
 import { PostAdminCreateBrand } from "./admin/brands/validators"
+import { z } from "zod"
+import { 
+  validateAndTransformQuery,
+} from "@medusajs/framework/http"
+import { createFindParams } from "@medusajs/medusa/api/utils/validators"
+// other imports...
 
+export const GetBrandsSchema = createFindParams()
 export default defineMiddlewares({
   routes: [
     {
@@ -13,5 +20,30 @@ export default defineMiddlewares({
         validateAndTransformBody(PostAdminCreateBrand),
       ],
     },
-  ],
+    {
+      matcher: "/admin/products",
+      method: ["POST"],
+      additionalDataValidator: {
+        brand_id: z.string().optional()
+      },
+   },
+   {
+      matcher: "/admin/brands",
+      method: "GET",
+      middlewares: [
+        validateAndTransformQuery(
+          GetBrandsSchema,
+          {
+            defaults: [
+              "id",
+              "name",
+              "products.*",
+            ],
+            isList: true,
+          }
+        ),
+      ],
+    },
+
+   ]
 })
